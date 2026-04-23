a8891c53a44a58f383bae8ee7bc0b12210987d8e
Hasan Abbas Sorathiya <hasanabbassorathiya@gmail.com>
2026-04-23 04:18:47 +0400

    fix: add missing firebase_core import to router

--- lib/shared/routes/router.dart
+++ lib/shared/routes/router.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
 import 'package:portfolio/shared/routes/app_routes.dart';
